d4fdcb66a55c2232e9b7d416433d474644c4106e
cyc3086 <36428206+cyc3086@users.noreply.github.com>
2019-07-12 12:58:48 -0500

    changed code to use simple-youtube-api

--- discord-bot/newbot.js
+++ discord-bot/newbot.js
@@ -1,9 +1,10 @@
 const Discord = require('discord.js');
 const auth = require('./auth.json');
 const ytdl = require('ytdl-core');
-const getJSON = require('get-json');
+const YouTube = require('simple-youtube-api');
 
 const bot = new Discord.Client();
+const yt = new YouTube(auth.apikey);
 bot.login(auth.token);
 
 bot.on('ready', async () => {
@@ -11,7 +12,7 @@ bot.on('ready', async () => {
     bot.user.setActivity('coolcatcoding! | `help');
 });
 
-bot.on('message', function (message) {
+bot.on('message', async message => {
     if (message.content.substring(0, 1) == '`') {
         var args = message.content.substring(1).split(' ');
         var cmd = args[0];
@@ -28,6 +29,7 @@ bot.on('message', function (message) {
                 break;
             case 'play':
                 message.channel.send('[play placeholder]');
+                console.log(args[0]);
                 if (!args[0]) {
                     message.channel.send('Please provide a link');
                     return;
@@ -36,27 +38,27 @@ bot.on('message', function (message) {
                     message.channel.send('You must be in a VC to listen');
                     return;
                 }
-                if (!servers[message.guild.id]) {
-                    servers[message.guild.id] = {
-                        queue: []
+                if (args[0].match(/^https?:\/\/(www.youtube.com|youtube.com)\/playlist(.*)$/)) {
+                    const playlist = await yt.getPlaylist(args[0]);
+                    const videos = await playlist.getVideos();
+                    for (const video of Object.values(videos)) {
+                        const vid = await yt.getVideoByID(video.id);
+                        await handle(vid, message);
                     }
+                    message.channel.send(`Added Playlist:${playlist.title} to the queue`);
+                } else if (args[0].match(/^.*(?:(?:youtu\.be\/|v\/|vi\/|u\/\w\/|embed\/)|(?:(?:watch)?\?v(?:i)?=|\&v(?:i)?=))([^#\&\?]*).*/)) {
+                    var video = await yt.getVideo(args[0]);
+                    await handle(video, message);
+                } else {
+                    message.channel.send(`${args[0]} is not a valid url.`);
                 }
-                var server = servers[message.guild.id];
-                server.queue.push(args[0]);
-                if (!message.guild.voiceConnection) message.member.voiceChannel.join().then(function (connection) {
-                    var id = args[0].match(/^.*(?:(?:youtu\.be\/|v\/|vi\/|u\/\w\/|embed\/)|(?:(?:watch)?\?v(?:i)?=|\&v(?:i)?=))([^#\&\?]*).*/)[1];
-                    getJSON('https://www.googleapis.com/youtube/v3/videos?part=id%2Csnippet&id=' + id + '&key=' + auth.apikey, function (error, data) {
-                        console.log('Now playing: ' + data.items[0].snippet.localized.title);
-                        message.channel.send('**Now playing:** *' + data.items[0].snippet.localized.title + '.*');
-                    });
-                    play(connection, message);
-                });
+                
                 break;
             case 'pause':
                 message.channel.send('[pause placeholder]');
                 break;
             case 'stop':
-                message.channel.send('[clear placeholder]');
+                message.channel.send('Stopped playing and cleared queue.');
                 var server = servers[message.guild.id];
                 if (message.guild.voiceConnection) {
                     for (var i = server.queue.length - 1; i >= 0; i--)
@@ -71,11 +73,15 @@ bot.on('message', function (message) {
                 if (server.dispatcher) server.dispatcher.end();
                 console.log('Skipped');
                 break;
+            case 'volume':
+                message.channel.send(`[volume placeholder]`);
+                if(!args[1]) message.channel.send
             case 'fuckyou':
                 message.channel.send('what time?');
                 break;
             default:
                 message.channel.send('Invalid Command');
+                break;
         }
     }
 });
@@ -87,5 +93,24 @@ function play(connection, message) {
     server.dispatcher.on('end', function () {
         if (server.queue[0]) play(connection, message);
         else connection.disconnect();
+    });
+}
+async function handle(video, message) {
+    const song = {
+        id: video.id,
+        title: video.title,
+        url: `https://www.youtube.com/watch?v=${video.id}`
+    };
+    if (!servers[message.guild.id]) {
+        servers[message.guild.id] = {
+            queue: []
+        };
+    }
+    var server = servers[message.guild.id];
+    server.queue.push(video.url);
+    if (!message.guild.voiceConnection) message.member.voiceChannel.join().then(function (connection) {
+        console.log(`Now playing: ${song.title}`);
+        message.channel.send(`**Now playing:** ${song.title}.`);
+        play(connection, message);
     });
 }
