40187e2a86aa1e656f46251e90b0838919391d7a
cyc3086 <36428206+cyc3086@users.noreply.github.com>
2020-03-08 16:53:46 -0500

    added comments

--- app/bot.js
+++ app/bot.js
@@ -6,13 +6,16 @@ const Discord = require('discord.js');
 const alpha  = require('alphavantage')({key: auth.alpha_vantage_key});
 const bot = new Discord.Client();
 
+// Channel IDs 
 const channel_ids = {
     'poll' : '686017506041004055'
 }
 
+// Authetication with discord
 bot.login(auth.discord_token);
 bot.on('ready', async () => {
     console.log(`${bot.user.username} is online.`);
+    // Set bot activity
     bot.user.setActivity(
         `ssp is obese | ${prefix}help`
     ).then(
@@ -20,6 +23,7 @@ bot.on('ready', async () => {
     ).catch(console.error);
 });
 
+// Prefix to communicate with bot
 const prefix = '&';
 
 // Commands- Key is command, value is help
@@ -27,7 +31,7 @@ var commands = {
     'help': 'Returns a list of commands.',
     'check': 'Returns stock data for the argument. Usage: `check <SYMBOL>`',
     'roll' : 'Returns a random number from 0 to <param> or <param1> to <param2> inclusive. Usage: `roll <MAX>` or `roll <MIN> <MAX>`',
-    'poll' : 'Creates a poll with the provided arguments as options'
+    'poll' : 'Creates a poll with the provided arguments as options. Usage: `poll "question" :emoji1: :emoji2:`'
 };
 
 // Gets formats date in YYYY-MM-DD
@@ -47,16 +51,25 @@ function formatDate(date) {
 
 // Bot responds to messages
 bot.on('message', async message => {
+    // Message Content
     var content = message.content;
 
+    // If it matches the prefix, treat is as commands
     if(content.substring(0,1) == prefix) {
+
+        // Gets arguments
         var args = content.substring(1).split(/\s+/);
         var cmd  = args[0];
 
         console.log(`Executed ${cmd} with parameter(s): ${args}`);
 
+        // Execute based on commands
         switch(cmd) {
+
+            // Help command
             case 'help' : 
+
+            // no arguments
             if(args.length < 2) {
                 var response = '';
                 var keys = Object.keys(commands);
@@ -64,19 +77,23 @@ bot.on('message', async message => {
                     response += keys[i] + '\n';
                 }
                 message.channel.send(`Valid commmands are:\n${response}`);
-            } else {
+            } else { // get help for specific command
                 message.channel.send(commands[args[1]]);
             }
                  break;
+            // Roll command
+
             case 'roll':
+                // 1 argument
                 if(args.length < 3) {
                     message.channel.send(`Rolled: ${Math.trunc(Math.random() * parseInt(args[1])) + 1}`);
                 }
-                else {
+                else { // More arguments
                     message.channel.send(`Rolled: ${Math.trunc(Math.random() * (parseInt(args[2]) - parseInt(args[1]) + 1)) + parseInt(args[1])}`);
                 }
                 break;
-            case 'check': alpha.data.quote(args[1]).then(data => {
+            // Check command
+                case 'check': alpha.data.quote(args[1]).then(data => {
                 // var today = formatDate(Date.now());
                 console.log(data);
                 data = data['Global Quote'];
@@ -87,14 +104,28 @@ bot.on('message', async message => {
                 }
                 message.channel.send(response);
             }); break;
-            case 'poll': 
-                var poll = content.match(/".+"/)[0].replace(/"/g,'');
-                let poll_msg = await bot.channels.get(channel_ids['poll']).send(poll);
-                var reactions = content.replace(/".+"/, '').split(/\s+/);
+
+            // Poll in current channel
+            case 'pollhere': 
+                var poll = content.match(/".+"/)[0].replace(/"/g,''); // Gets poll question
+                var poll_msg = await message.channel.send(poll); // Sends message
+                var reactions = content.replace(/".+"/, '').split(/\s+/); // Gets arguments 
                 for(var i = 1; i < reactions.length; i++) {
-                    await poll_msg.react(reactions[i]);
+                    await poll_msg.react(reactions[i]); // Adds reactions
                 }
             break;
+            
+            // Poll in poll channel
+            case 'poll': 
+                var poll = content.match(/".+"/)[0].replace(/"/g,''); // Gets poll question
+                var poll_msg = await bot.channels.get(channel_ids['poll']).send(poll); // sends
+                var reactions = content.replace(/".+"/, '').split(/\s+/); // gets arguemnts
+                for(var i = 1; i < reactions.length; i++) {
+                    await poll_msg.react(reactions[i]); // adds reaction
+                }
+            break;
+
+            // Handle invalid commands
             default: message.channel.send(`'${cmd}' is not a valid command.`); break;
         }
     }
